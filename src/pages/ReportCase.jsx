13719a065338a71305bf4c8db531e40880711fd9
Ccode-400 <stanleymngige@gmail.com>
2026-05-14 16:29:31 +0300

    add ReportCase return display

---
+++ src/pages/ReportCase.jsx
@@ -0,0 +1,7 @@
+function ReportCase() {
+  return (
+    <div className="p-10 max-w-2xl mx-auto">
+      <h1 className="text-4xl font-bold mb-6 text-center">
+        Report Animal Cruelty
+      </h1>
+      
