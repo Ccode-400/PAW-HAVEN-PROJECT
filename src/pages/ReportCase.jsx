41c5f311e8a7a4fc22b2973c94ac412d90c6af1f
Ccode-400 <stanleymngige@gmail.com>
2026-05-15 02:22:52 +0300

    add report form display

--- src/pages/ReportCase.jsx
+++ src/pages/ReportCase.jsx
@@ -1,1 +1,16 @@
 import "../styles/ReportCase.css";
+
+function ReportCase() {
+  return (
+    <div className="report-container">
+      <h1>Report Animal Cruelty</h1>
+
+      <form className="report-form">
+        <input
+          type="text"
+          placeholder="Location"
+        />
+
+        <textarea
+          placeholder="Describe the incident"
+        ></textarea>
